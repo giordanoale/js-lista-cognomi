a780445662566bd2c6f28cce8503d8b2715cfa46
giordanoale <a.giordano89@gmail.com>
2021-03-03 16:08:15 +0100

    Il programma aggiunge il cognome dell'utente e lo inserisce nell'array, successivamente scrive in console log l'array in formato alfabetico

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-alert();
+// Chiedere all’utente il cognome
+
+var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi", "Gastani Frinzi"];
+
+var utente = prompt("Inserisci il tuo cognome");
+
+// Inserirlo in un array con altri cognomi:
+
+cognomi.push(utente);
+
+// Stampa la lista ordinata alfabeticamente
+
+cognomi.sort();
+console.log(cognomi);
+
+// Scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova

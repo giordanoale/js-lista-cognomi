79dd69e49ad5efaa63cef25d9fda27d23f5164d4
giordanoale <a.giordano89@gmail.com>
2021-03-03 16:22:18 +0100

    Il programma scrive nel console log la posizione del cognome dell'utente

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 // Chiedere all’utente il cognome
 
-var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi", "Gastani Frinzi"];
+var cognomi = ["bianchi", "rossi", "duzioni", "balsano", "verdi", "gastani frinzi"];
 
 var utente = prompt("Inserisci il tuo cognome");
 
-// Inserirlo in un array con altri cognomi:
+// Inserirlo in un array con altri cognomi
 
 cognomi.push(utente);
 
@@ -14,3 +14,12 @@ cognomi.sort();
 console.log(cognomi);
 
 // Scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+
+for (var i = 0; i < cognomi.length; i++) {
+
+  if (utente == cognomi[i]) {
+    var posizione = i + 1;
+    console.log("La posizione di " + utente + " all'interno dell'Array è la numero " + posizione);
+  }
+
+}
